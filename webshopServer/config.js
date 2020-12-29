fba699126691537eddfd628433461bfbc7c13017
Hong Duc Cai <hongduccai@gmail.com>
2020-12-29 22:38:27 +0100

    Delete config.js

--- webshopServer/config.js
+++
@@ -1,7 +0,0 @@
-var config = {}
-
-config.port = process.env.PORT || 5000
-
-config.database = 'mongodb+srv://mester:ducduc12@hong.qravj.mongodb.net/Webshop?retryWrites=true&w=majority'
-
-module.exports = config
