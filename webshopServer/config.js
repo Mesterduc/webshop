625d5f190c22d0669278acb7e8a71deb18416d77
Hong Duc Cai <hong.duc.cai@gmail.com>
2020-12-29 22:22:41 +0100

    ad
    asd

---
+++ webshopServer/config.js
@@ -0,0 +1,7 @@
+var config = {}
+
+config.port = process.env.PORT || 5000
+
+config.database = 'mongodb+srv://mester:ducduc12@hong.qravj.mongodb.net/Webshop?retryWrites=true&w=majority'
+
+module.exports = config
